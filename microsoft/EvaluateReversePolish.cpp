7c0a42f2c6c961537b6f3082f73cc089c99015a8
Sagar-Arora-eng <85285942+Sagar-Arora-eng@users.noreply.github.com>
2023-01-04 13:53:18 +0530

    solution of evaluate reverse polish notation problem

---
+++ microsoft/EvaluateReversePolish.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int evalRPN(vector<string>& tokens) {
+
+    int n = tokens.size(); 
+
+    stack<string> stk; 
+    for(int i=0; i<n; i++){
+        if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/"){
+            int operand2 = stoi(stk.top()); stk.pop(); 
+            int operand1 = stoi(stk.top()); stk.pop(); 
+            int res; 
+            if(tokens[i] == "+") res =  operand1 + operand2; 
+            if(tokens[i] == "-") res =  operand1 - operand2;
+            if(tokens[i] == "*") res =  operand1 * operand2;
+            if(tokens[i] == "/") res =  operand1 / operand2;
+            stk.push(to_string(res)); 
+        }else{
+            stk.push(tokens[i]); 
+        }
+    }
+    int ans = stoi(stk.top()); 
+    stk.pop(); 
+
+    return ans; 
+}
+
+int main(){
+   vector<string> tokens = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"}; 
+   int n = tokens.size(); 
+
+   cout << evalRPN(tokens) << endl;
+
+   return 0;
+}
